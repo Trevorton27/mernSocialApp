6dee2272fdc2b9ef9702a3d4b8952880da1ee625
Trevor Mearns <spiral272@gmail.com>
2022-01-30 12:48:13 +0900

    register and login working in Postman

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const multer = require('multer');
-const userRoute = require('./routes/users');
 
-const postRoute = require('./routes/posts');
 const router = express.Router();
 const path = require('path');
 const PORT = 8800;
@@ -25,34 +23,13 @@ mongoose.connect(
     else console.log('mongdb is connected');
   }
 );
-// app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   try {
-//     return res.status(200).json("File uploded successfully");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
-app.use('/api/users', userRoute);
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/posts', postRoute);
 
 app.listen(PORT, () => {
   console.log('Backend server is running!');
